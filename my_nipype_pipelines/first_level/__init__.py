368b7f32c1c52dae1ed2bf28503765c5f248eaa4
jrasero <jrasero.daparte@gmail.com>
2021-09-28 11:48:00 -0400

    fsl pipeline and smoothing operation has been simplified with only one node, because images to smooth are previously merged in the same list

--- my_nipype_pipelines/first_level/__init__.py
+++ my_nipype_pipelines/first_level/__init__.py
@@ -1,1 +1,4 @@
-from my_nipype_pipelines.first_level.workflows import create_first_level_spm_wls_wf
+from my_nipype_pipelines.first_level.workflows import (
+    create_first_level_spm_wf,
+    create_first_level_fsl_wf
+    )
--- my_nipype_pipelines/first_level/workflows.py
+++ my_nipype_pipelines/first_level/workflows.py
@@ -2,16 +2,17 @@ from my_nipype_pipelines.first_level.utils \
     import (create_contrasts, create_subject_info_from_bids, smooth_imgs)
 
 
-def create_first_level_spm_wls_wf(bold_img,
-                                  confounders_file,
-                                  events_file,
-                                  output_dir,
-                                  repetition_time,
-                                  mask_file=None,
-                                  confounders=None,
-                                  contrasts=None,
-                                  high_pass=None,
-                                  fwhm=None):
+def create_first_level_spm_wf(bold_img,
+                              confounders_file,
+                              events_file,
+                              output_dir,
+                              repetition_time,
+                              robuts_wls=True,
+                              mask_file=None,
+                              confounders=None,
+                              contrasts=None,
+                              high_pass=None,
+                              fwhm=None):
     """
     Parameters
     ----------
@@ -25,6 +26,8 @@ def create_first_level_spm_wls_wf(bold_img,
         Directory where to store output first-level estimations.
     repetition_time : float
         Repetition time of the acquisition.
+    robust_wls: bool
+        Model estimation using RobustWLS toolbox or not. The default is True.
     mask_file : nifti file, optional
         Image for explicitly masking the analysis. The default is None.
     confounders : {List of strings or None}, optional
@@ -50,7 +53,7 @@ def create_first_level_spm_wls_wf(bold_img,
     from nipype.algorithms.misc import Gunzip
     from nipype.interfaces import spm
     from nipype.interfaces import io as nio
-    from nipype.interfaces.utility import Function
+    from nipype.interfaces.utility import Function, Merge
     from my_nipype_pipelines.first_level.interfaces import Level1DesignWLS
 
     # TODO: Maybe it's more convenient to ask to pass directly a data frame.
@@ -73,7 +76,11 @@ def create_first_level_spm_wls_wf(bold_img,
         except ValueError:
             raise print(f"error convert {fwhm} to float")
 
-    wf = pe.Workflow(name="first_level_spm_wls")
+    if robuts_wls:
+        wf = pe.Workflow(name="first_level_spm_wls")
+    else:
+        wf = pe.Workflow(name="first_level_spm")
+
     gun_zip = pe.Node(Gunzip(in_file=bold_img),
                       name="unzip_nifti")
 
@@ -90,11 +97,18 @@ def create_first_level_spm_wls_wf(bold_img,
     wf.connect(gun_zip, 'out_file', specify_model, 'functional_runs')
 
     # TODO: pass bases as input argument in function
-    design = pe.Node(Level1DesignWLS(interscan_interval=repetition_time,
-                                     bases={'hrf': {'derivs': [0, 0]}},
-                                     model_serial_correlations='wls',
-                                     timing_units='secs'),
-                     name="level1_design")
+    if robuts_wls:
+        design = pe.Node(Level1DesignWLS(interscan_interval=repetition_time,
+                                         bases={'hrf': {'derivs': [0, 0]}},
+                                         model_serial_correlations='wls',
+                                         timing_units='secs'),
+                         name="level1_design")
+    else:
+        design = pe.Node(spm.Level1Design(interscan_interval=repetition_time,
+                                          bases={'hrf': {'derivs': [0, 0]}},
+                                          model_serial_correlations='AR(1)',
+                                          timing_units='secs'),
+                         name="level1_design")
 
     wf.connect(specify_model, 'session_info',  design, 'session_info')
 
@@ -102,7 +116,6 @@ def create_first_level_spm_wls_wf(bold_img,
         gun_zip_mask = pe.Node(Gunzip(in_file=mask_file),
                                name="unzip_mask")
         wf.connect(gun_zip_mask, 'out_file',  design, 'mask_image')
-
 
     # TODO: The same as above regarding inputs
     estimate_model = pe.Node(
@@ -123,26 +136,22 @@ def create_first_level_spm_wls_wf(bold_img,
     wf.connect(estimate_model, "residual_image",
                estimate_contrasts, "residual_image")
 
-    smoothing_betas = pe.Node(Function(input_names=["imgs", "fwhm"],
-                                       output_names=["smoothed_betas"],
-                                       function=smooth_imgs),
-                              name="smooth_betas")
-    smoothing_betas.inputs.fwhm = fwhm
-    wf.connect(estimate_model, "beta_images", smoothing_betas, "imgs")
+    merge_estimations = pe.Node(Merge(numinputs=3),
+                                name="merge_estimations")
 
-    smoothing_contrasts = pe.Node(Function(input_names=["imgs", "fwhm"],
-                                           output_names=["smoothed_cons"],
-                                           function=smooth_imgs),
-                                  name="smooth_contrasts")
-    smoothing_contrasts.inputs.fwhm = fwhm
-    wf.connect(estimate_contrasts, "con_images", smoothing_contrasts, "imgs")
+    wf.connect(estimate_model, "beta_images", merge_estimations, "in1")
+    wf.connect([(estimate_contrasts, merge_estimations,
+                 [("con_images", "in2"),
+                  ("spmT_images", "in3")])
+                ])
 
-    smoothing_stats = pe.Node(Function(input_names=["imgs", "fwhm"],
-                                       output_names=["smoothed_stats"],
-                                       function=smooth_imgs),
-                              name="smooth_stats")
-    smoothing_stats.inputs.fwhm = fwhm
-    wf.connect(estimate_contrasts, "spmT_images", smoothing_stats, "imgs")
+    smoothing_estimations = pe.Node(
+        Function(input_names=["imgs", "fwhm"],
+                 output_names=["smoothed_estimations"],
+                 function=smooth_imgs),
+        name="smooth_estimations")
+    smoothing_estimations.inputs.fwhm = fwhm
+    wf.connect(merge_estimations, "out", smoothing_estimations, "imgs")
 
     # Save data
     datasink = pe.Node(nio.DataSink(base_directory=output_dir),
@@ -154,11 +163,141 @@ def create_first_level_spm_wls_wf(bold_img,
                             ("spmT_images", "@foo.@spmT_images"),
                             ("spm_mat_file", "@foo.@spm_mat_file")])
                 ])
-    wf.connect(smoothing_betas, "smoothed_betas",
-               datasink, "@foo.@smoothed_betas")
-    wf.connect(smoothing_contrasts, "smoothed_cons",
-               datasink, "@foo.@smoothed_cons")
-    wf.connect(smoothing_stats, "smoothed_stats",
-               datasink, "@foo.@smoothed_stats")
+
+    wf.connect(smoothing_estimations, "smoothed_estimations",
+               datasink, "@foo.@smoothed_estimations")
 
     return wf
+
+
+def create_first_level_fsl_wf(bold_img,
+                              confounders_file,
+                              events_file,
+                              output_dir,
+                              repetition_time,
+                              mask_file=None,
+                              confounders=None,
+                              contrasts=None,
+                              high_pass=None,
+                              fwhm=None):
+
+    from nipype.pipeline import engine as pe
+    import nipype.algorithms.modelgen as model
+
+    from nipype.interfaces import fsl
+    from nipype.interfaces import io as nio
+    from nipype.interfaces.utility import Function, Merge
+
+    # TODO: Maybe it's more convenient to ask to pass directly a data frame.
+    # as confounders (with column names). This way would be more general,
+    # not restricted to only fmriprep like confounders files.
+    if confounders is None:
+        motion = ['trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z']
+        confounders = motion
+
+    subject_info = create_subject_info_from_bids(events=events_file,
+                                                 confounds=confounders_file,
+                                                 regressor_names=confounders)
+
+    if contrasts is None:
+        contrasts = create_contrasts(events=events_file)
+
+    if fwhm:
+        try:
+            fwhm = float(fwhm)
+        except ValueError:
+            raise print(f"error convert {fwhm} to float")
+
+    wf = pe.Workflow(name="first_level_fsl")
+
+    # If mask file, apply mask to input bold data
+    if mask_file:
+        mask_bold = pe.Node(fsl.maths.ApplyMask(in_file=bold_img,
+                                                mask_file=mask_file),
+                            name="mask_bold")
+    else:
+        mask_bold = pe.Node(pe.utils.IdentityInterface(fields=["out_file"]),
+                            name="mask_bold_nomask")
+        mask_bold.inputs.out_file = bold_img
+
+    # Node to specify the FSL Model
+    specify_model = pe.Node(
+        model.SpecifyModel(parameter_source='FSL',
+                           input_units='secs',
+                           high_pass_filter_cutoff=high_pass,
+                           time_repetition=repetition_time,
+                           subject_info=subject_info),
+        name="specify_model")
+
+    wf.connect(mask_bold, 'out_file', specify_model, "functional_runs")
+
+    # Node to generate the FEAT specific files
+    design = pe.Node(fsl.Level1Design(bases={'dgamma': {'derivs': False}},
+                                      model_serial_correlations=True,
+                                      contrasts=contrasts,
+                                      interscan_interval=repetition_time),
+                     name="design")
+
+    wf.connect(specify_model, "session_info", design, "session_info")
+
+    feat = pe.Node(fsl.FEAT(), name='FEAT')
+    wf.connect(design, "fsf_files", feat, "fsf_file")
+
+    feat_select = pe.Node(nio.SelectFiles({
+        'param_estimates': 'stats/pe*.nii.gz',
+        'copes': 'stats/cope*.nii.gz',
+        'varcopes': 'stats/varcope*.nii.gz',
+        'tstats': 'stats/tstat*.nii.gz',
+        'zstats': 'stats/zstat*.nii.gz'}),
+        name='feat_select')
+
+    wf.connect(feat, "feat_dir", feat_select, "base_directory")
+
+    merge_estimations = pe.Node(Merge(numinputs=5),
+                                name="merge_estimations")
+
+    wf.connect([(feat_select, merge_estimations,
+                 [("param_estimates", "in1"),
+                  ("copes", "in2"),
+                  ("varcopes", "in3"),
+                  ("tstats", "in4"),
+                  ("zstats", "in5")])
+                ])
+
+    smoothing_estimations = pe.Node(
+        Function(input_names=["imgs", "fwhm"],
+                 output_names=["smoothed_estimations"],
+                 function=smooth_imgs),
+        name="smooth_estimations")
+    smoothing_estimations.inputs.fwhm = fwhm
+    wf.connect(merge_estimations, "out", smoothing_estimations, "imgs")
+
+    # Save data
+    datasink = pe.Node(nio.DataSink(base_directory=output_dir),
+                       name='data_sink')
+
+    wf.connect(feat_select, "param_estimates", datasink, "@foo")
+    wf.connect([(feat_select,
+                 datasink, [("copes", "@foo.@copes"),
+                            ("varcopes", "@foo.@varcopes"),
+                            ("tstats", "@foo.@tstats"),
+                            ("zstats", "@foo.@zstats")])
+                ])
+    wf.connect(smoothing_estimations, "smoothed_estimations",
+               datasink, "@foo.@smoothed_estimations")
+
+    return wf
+
+
+def create_first_level_nilearn_wf(bold_img,
+                                  confounders_file,
+                                  events_file,
+                                  output_dir,
+                                  repetition_time,
+                                  mask_file=None,
+                                  confounders=None,
+                                  contrasts=None,
+                                  high_pass=None,
+                                  fwhm=None):
+    raise NotImplementedError
+
